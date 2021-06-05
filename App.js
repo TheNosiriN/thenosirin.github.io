2f61ca6710c8d14f9b2d16dc6ac7c1e08518a785
TheNosiriN <thenosirin@gmail.com>
2021-06-05 13:10:12 -0400

    fonts

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import { el, mount } from "https://redom.js.org/redom.es.min.js";
 
 
 
-const hello = el("h1", "Hello world!");
-
-
-mount(document.body, hello);
-
-console.log(hello.textContent);
+// const hello = el("h1", "Hello world!");
+//
+//
+// mount(document.body, hello);
+//
+// console.log(hello.textContent);
